3e9f5fd21ff43c32c682c2998151e9f08d9c8943
Jesús Trinidad Díaz Ramírez <jesdazrez@gmail.com>
2021-11-10 11:54:34 -0600

    docs: Add enum example

---
+++ examples/enum.rs
@@ -0,0 +1,10 @@
+use ddc::Ddc;
+use ddc_winapi::Monitor;
+
+fn main() {
+    let monitors = Monitor::enumerate().unwrap();
+    for mut m in monitors {
+        print!("{:?}: ", m);
+        println!("{:?}", m.get_timing_report());
+    }
+}
